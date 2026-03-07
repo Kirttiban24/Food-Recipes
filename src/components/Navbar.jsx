6445383e328aabbe1abbe537439e493a8ae5015d
Kirttiban24 <153419795+Kirttiban24@users.noreply.github.com>
2026-03-07 17:07:53 +0530

    project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-center gap-x-10 text-sm mb-10 '>
+      <NavLink 
+        className={(e) => e.isActive ? "text-red-400" : ""}
+        to='/'
+        >
+          Home
+      </NavLink>
+
+      <NavLink 
+        className={(e) => e.isActive ? "text-red-400" : ""}
+        to='/recipes'
+        >
+          Recipes
+      </NavLink>      
+
+      <NavLink 
+        className={(e) => e.isActive ? "text-red-400" : ""} 
+        to='/about'
+        >
+          About
+      </NavLink>
+
+      <NavLink 
+        className={`px-4 py-2 bg-gray-900 rounded ${(e) => 
+          e.isActive ? "text-red-400" : ""}`} 
+        to='/create-recipes'
+        >
+          Create Recipes
+      </NavLink>      
+    </div>
+  )
+}
+
+export default Navbar
