84dbf5a39e0aaa12d031c9f1f25eb6604d97af42
Kirttiban24 <153419795+Kirttiban24@users.noreply.github.com>
2026-03-22 12:45:52 +0530

    update create section

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,15 +1,82 @@
-import { useContext } from 'react'
-import { useForm } from "react-hook-form";
-import { recipecontext } from '../context/RecipeContext';
-import { nanoid } from 'nanoid'
-import { toast } from 'react-toastify';
-import { useNavigate } from 'react-router-dom';
+import { useContext } from "react"
+import { recipecontext } from "../context/RecipeContext"
+import { useForm } from "react-hook-form"
+import { nanoid } from "nanoid"
+import { toast } from "react-toastify"
+import { useNavigate } from "react-router-dom"
+
 const Create = () => {
 
+  const navigate = useNavigate()
+  const { data, setdata } = useContext(recipecontext)
+  const { register, handleSubmit, reset } = useForm()
 
+  const submitHandler = (recipe) => {
+    recipe.id = nanoid()
+    setdata([...data, recipe]);
+    toast.success("New Recipe Created")
+    reset();
+    navigate("/recipes")
+  }
 
   return (
-    <div></div>
+    <form onSubmit={handleSubmit(submitHandler)}>
+              <input
+                className='block border-b outline-0 p-2' 
+                {...register("image")} 
+                type="url"
+                placeholder='Enter Image Url'  
+              />
+            <small className='text-red-500'>
+                This is an Error
+            </small>
+            
+            <input
+                className='block border-b outline-0 p-2' 
+                {...register("title")}
+                type="text" 
+                placeholder='Recipe Title' 
+            />
+
+            <input
+                className='block border-b outline-0 p-2' 
+                {...register("chef")}
+                type="text" 
+                placeholder='Chef Name' 
+            />
+
+            <textarea
+                className='block border-b outline-0 p-2' 
+                {...register("desc")}
+                placeholder='Start from here' 
+            ></textarea>
+
+            <textarea
+                className='block border-b outline-0 p-2' 
+                {...register("ingr")}  
+                placeholder='Write ingrediants separated by comma' 
+            ></textarea>
+
+            <textarea
+                className='block border-b outline-0 p-2' 
+                {...register("inst")}  
+                placeholder='Write instruction separated by comma' 
+            ></textarea>
+
+            <select
+                className='block border-b outline-0 p-2' 
+                {...register("category")}  
+            >
+                <option className='text-black' value="Breakfast">Breakfast</option>
+                <option className='text-black' value="Lunch">Lunch</option>
+                <option className='text-black' value="Supper">Supper</option>
+                <option className='text-black' value="Dinner">Dinner</option>
+            </select>
+
+            <button className="mt-5 block bg-gray-900 cursor-pointer px-4 py-2 rounded">
+              Save Recipe
+            </button>      
+      </form>
   )
 }
 
